294579ad4e89f8724e56013a0eaed47b37882c10
unknown <neiluisjr@gmail.com>
2022-06-02 22:13:35 -0300

    labetube

---
+++ modulo2/css-labetube/index.js
@@ -0,0 +1,18 @@
+
+  const animais = [
+    { nome: "Cachorro", classificacao: "mamífero" },
+    { nome: "Papagaio", classificacao: "ave" },
+    { nome: "Gato", classificacao: "mamífero" },
+    { nome: "Carpa", classificacao: "peixe" },
+    { nome: "Pomba", classificacao: "ave" }
+  ]
+
+
+const nomeAnimais = animais.map((item) =>{
+return item.nome
+})
+
+console.log(nomeAnimais)
+
+
+
