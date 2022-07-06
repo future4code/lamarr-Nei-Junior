79bd68ad59b42cf5f40e871c0253b4b468361539
unknown <neiluisjr@gmail.com>
2022-07-05 22:50:00 -0300

    Adicionando lista

--- modulo3/insta4/insta4/src/App.js
+++ modulo3/insta4/insta4/src/App.js
@@ -2,19 +2,40 @@ import React from 'react';
 import Post from './components/Post/Post';
 import './style.css'
 
-
 function App() {
-return(
-  <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-  </div>
-)
 
+const transfArray = [
+  {
+    nomeUsuario: 'paulinha',
+    fotoUsuario: 'https://picsum.photos/50/50',
+    fotoPost: 'https://picsum.photos/200/150',
+  },
+  {
+    nomeUsuario: 'rafael',
+    fotoUsuario: 'https://picsum.photos/50/50',
+    fotoPost: 'https://picsum.photos/200/150',
+  },
+  {
+    nomeUsuario: 'sarah',
+    fotoUsuario: 'https://picsum.photos/50/50',
+    fotoPost: 'https://picsum.photos/200/150',
+  }
+]
+
+const mapeandoArray = transfArray.map((post, index) => {
+
+
+  return (
+      <Post
+        nomeUsuario={post.nomeUsuario}
+        fotoUsuario={post.fotoUsuario}
+        fotoPost={post.fotoPost}
+      />
+  )
+
+})
+
+
+  return <li>{mapeandoArray}</li>
 }
-
-
 export default App;
