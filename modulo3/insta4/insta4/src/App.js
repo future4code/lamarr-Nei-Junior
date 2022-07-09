f1b9ffe896bdaad0de00fdfe088ed167a2f11820
unknown <neiluisjr@gmail.com>
2022-07-08 23:50:47 -0300

    mudanças

--- modulo3/insta4/insta4/src/App.js
+++ modulo3/insta4/insta4/src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
 import './style.css'
 
-function App() {
 
-const transfArray = [
+
+function App() {<>
+
+<input
+placeholder='Nome'
+value={inputName}
+onChange={handleInputName} 
+/>
+
+<input
+placeholder='Foto'
+value={inputfoto}
+onChange={handleInputFoto} 
+/>
+
+<button onClick={addPost} />
+
+
+const handleInputName = (e) => { 
+setName(e.target.value)
+}
+const handleInputFoto = (e) => {
+setForo(e.target.value)
+}
+
+
+</>
+
+const [inputName, setName] = useState("")
+const [inputfoto, setForo] = useState("")
+const [transfArray, setPost ] = useState ([
   {
     nomeUsuario: 'paulinha',
     fotoUsuario: 'https://picsum.photos/50/50',
@@ -20,7 +49,14 @@ const transfArray = [
     fotoUsuario: 'https://picsum.photos/50/50',
     fotoPost: 'https://picsum.photos/200/150',
   }
-]
+])
+
+const addPost = () =>{
+const novoPost = {nomeUsuario:inputName,fotoUsuario:inputfoto}
+const novaListaPosts = [... transfArray, novoPost]
+setPost(novaListaPosts)
+
+}
 
 const mapeandoArray = transfArray.map((post, index) => {
 
@@ -34,8 +70,10 @@ const mapeandoArray = transfArray.map((post, index) => {
   )
 
 })
-
-
   return <li>{mapeandoArray}</li>
+  
 }
+
+
+
 export default App;
