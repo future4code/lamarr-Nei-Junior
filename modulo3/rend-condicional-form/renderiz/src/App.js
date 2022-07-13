a87209f331ac842e12842dffdbf489fae294cb3d
unknown <neiluisjr@gmail.com>
2022-07-12 22:03:37 -0300

    Nao rolou

--- modulo3/rend-condicional-form/renderiz/src/App.js
+++ modulo3/rend-condicional-form/renderiz/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './App.css';
 import Etapa1 from './componentes/Etapa1'
 import Etapa2 from './componentes/Etapa2'
@@ -6,15 +6,31 @@ import Etapa3 from './componentes/Etapa3'
 import EtapaFinal from './componentes/EtapaFinal';
 import styled, { StyledComponent } from "styled-components";
 
+// Seletor
+
+function SeletorPagina (){
+const [seletor, setTrocaEtapa] = useState(1)
+
+  switch (seletor) {
+    case 1: return <Etapa1 />
+    case 2: return <Etapa2 />
+    case 3: return <Etapa3 />
+    case 4: return <EtapaFinal />
+  }
+}
 
 
 function App() {
+
+
+
   return (
-    <div>      
+    <div>
       <button>Próxima etapa</button>
-      <Etapa1/>
-      <Etapa2/>
+      <Etapa1 />
+
     </div>
   );
 }
+
 export default App;
