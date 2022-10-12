46dc6376c577a845e5a99db53285072630dede4d
unknown <neiluisjr@gmail.com>
2022-10-11 23:08:10 -0300

    Exercicios node

---
+++ modulo 5/index.js
@@ -0,0 +1,45 @@
+// Exercicio 1
+
+
+// const nome= process.argv[2];
+// const idade = Number(process.argv[3]);
+// const idade7anos = Number(process.argv[3])+7
+
+// function nomeIdade (nome, idade) {
+
+// return `meu nome é ${nome} e minha idade é ${idade} e em 7 anos terei ${idade7anos}`
+// }
+
+// const resposta = nomeIdade(nome, idade)
+
+// console.log(resposta)
+
+
+// Exercicio 2
+
+// const string = String(process.argv[2])
+// const n1 = Number(process.argv[3])
+// const n2 = Number(process.argv[4])
+
+// let operacao 
+
+// switch(string){
+// 	case "soma":
+//     console.log(n1+n2)
+//     break;
+
+// 	case "subt":
+//     console.log(n1-n2)
+// 	break;
+// }
+
+
+// // Exercicio 3 
+
+// const lista = ["Acordar", "Beber Agua", "Comer", "Estudar", "Trabalhar", "Academia"]
+// const tarefa = process.argv[2]
+
+// const novalista = [...lista, tarefa]
+
+// console.log("Parabens Incluiu uma nova Tarefa")
+// console.log(novalista)
